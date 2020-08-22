cfa4a1e6e98c96c986145f0275283219b55482d5
Justinriverng <Justinriverng@gmail.com>
2020-08-22 15:47:34 -0700

    Functional code for adding, subtracting, and multiplying 2 numbers in the console app.

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -10,6 +10,37 @@ namespace FirstConsoleApplication
     {
         static void Main(string[] args)
         {
+            /** Variable Declarations. */
+            int num1, num2;
+            int result = 0;
+
+            /** Prompt the user for 2 numbers. */
+            Console.Write("Enter your first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            /** Prompt the user for operation choice. */
+            Console.WriteLine("Your choices are:\n1.Add 2.Subtract 3.Multiply");
+            Console.Write("Enter your operation choice: ");
+            int operationChoice = Convert.ToInt32(Console.ReadLine());
+
+            /** Determine the operation choice. */
+            if (operationChoice == 1)
+            {
+                result = num1 + num2;
+            }
+            else if (operationChoice == 2)
+            {
+                result = num1 - num2;
+            }
+            else if (operationChoice == 3)
+            {
+                result = num1 * num2;
+            }
+            Console.WriteLine(result);
+            Console.WriteLine("Press enter to close the console.");
+            Console.ReadLine();
         }
     }
 }
